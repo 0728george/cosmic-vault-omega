c5513e39c6f7252ea2feb2395988988849354b71
Gheorghe Varga <149835163+0728george@users.noreply.github.com>
2025-11-18 21:12:34 -0500

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        orbitron: ['Orbitron', 'sans-serif'],
+        exo: ['Exo 2', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
