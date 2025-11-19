8ce7aece62996bc5cd578e8665ffe1ec2646e24f
Gheorghe Varga <149835163+0728george@users.noreply.github.com>
2025-11-18 21:15:48 -0500

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Header from "@/components/Header";
+import Search from "@/components/Search";
+
+export default function Home() {
+  return (
+    <main className="relative z-10 flex min-h-screen flex-col items-center justify-center px-6 text-center">
+      <Header />
+      <Search />
+    </main>
+  );
+}
